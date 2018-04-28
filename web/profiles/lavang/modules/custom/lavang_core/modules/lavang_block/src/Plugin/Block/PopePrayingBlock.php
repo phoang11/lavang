bd72559a39bffc9837b046b6a0ba1ad83defef3d
Phuoc Hoang <ph@PTH.local>
2018-04-27 20:29:47 -0400

    add lavang_core, lavang_block modules

---
+++ web/profiles/lavang/modules/custom/lavang_core/modules/lavang_block/src/Plugin/Block/PopePrayingBlock.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Drupal\lavang_block\Plugin\Block;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a 'PopePrayingBlock' block.
+ *
+ * @Block(
+ *   id = "lavang_block_pope_praying",
+ *   admin_label = @Translation("Pope Praying"),
+ *   category = @Translation("Lavang Block")
+ * )
+ */
+class PopePrayingBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
+   * Constructs a new PopePrayingBlock instance.
+   *
+   * @param array $configuration
+   *   The plugin configuration, i.e. an array with configuration values keyed
+   *   by configuration option name. The special key 'context' may be used to
+   *   initialize the defined contexts by setting it to an array of context
+   *   values keyed by context names.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The route match.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, RouteMatchInterface $route_match) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->routeMatch = $route_match;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_route_match')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'content' => '<h4>Tháng Tư, 2018 <br > <br > Cầu nguyện cho giới hữu trách về nền kinh tế</h4>
+      <p>
+        Chúng ta hãy cùng nhau lên tiếng, để yêu cầu các nhà kinh tế biết can đảm từ chối nền kinh tế loại trừ con người, và biết cách mở ra những con đường mới.
+      </p>',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form['content'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Block content'),
+      '#default_value' => $this->configuration['content'],
+      '#required' => TRUE,
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['content'] = $form_state->getValue('content');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build['content'] = [
+      '#markup' => $this->configuration['content'],
+    ];
+    return $build;
+  }
+
+}
