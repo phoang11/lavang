48c723cb6edd3cd12e6b5d0e9de5912b8396409b
Phuoc Hoang <ph@PTH.local>
2018-10-31 21:30:20 -0400

    update drupal core 8.6.2

---
+++ web/profiles/lavang/modules/custom/lavang_core/src/Plugin/Block/SidebarNavigationBlock.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Drupal\lavang_core\Plugin\Block;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\node\Entity\Node;
+use Drupal\system\Entity\Menu;
+use Drupal\system\Plugin\Block\SystemMenuBlock;
+
+
+
+/**
+ * Provides a 'Sidebar Navigation' block.
+ *
+ * @Block(
+ *   id = "lavang_core_sidebar_navigation",
+ *   admin_label = @Translation("Sidebar Navigation"),
+ *   category = @Translation("Lavang")
+ * )
+ */
+class SidebarNavigationBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
+   * Constructs a new SidebarNavigationBlock instance.
+   *
+   * @param array $configuration
+   *   The plugin configuration, i.e. an array with configuration values keyed
+   *   by configuration option name. The special key 'context' may be used to
+   *   initialize the defined contexts by setting it to an array of context
+   *   values keyed by context names.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The route match.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, RouteMatchInterface $route_match) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->routeMatch = $route_match;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_route_match')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'content' => $this->t('Hello world!'),
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form['content'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Block content'),
+      '#default_value' => $this->configuration['content'],
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['content'] = $form_state->getValue('content');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $build['content'] = [
+      '#markup' => $markup,
+    ];
+    return $build;
+  }
+
+}
